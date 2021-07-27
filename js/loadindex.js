a88abcf2b8dafbdb02515aa5670edc2baea1b727
AEight1973 <1015252562@163.com>
2021-07-27 18:25:41 +0800

    Changelist from Workplace

--- js/loadindex.js
+++ js/loadindex.js
@@ -1,7 +1,26 @@
+
 const url_api = 'www.aeight.cn/api/'
 
 function load_latestarctile(){
-
+    $.ajax({
+        type: "POST",
+        data: data,
+        url: "https://gischaser.cn/pensionweb_backend/suggest",
+        dataType: 'json',
+        crossDomain: true,
+        success: function(data) {
+            console.log("ajax success");
+            if(data.code === '500000') {
+                SuggestList = data.message;
+                UpdateSuggest();
+                PulsingScatter();
+                UpdateBounds();
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus + ' ' + errorThrown);
+        }
+    })
 }
 
 function load_latestupdate(){
